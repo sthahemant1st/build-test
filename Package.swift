92b3f6c7daa147b4fcf43f9522fd0caede1eadd5
Hemant Shrestha <sthahemant1st@gmail.com>
2025-05-12 13:24:18 +0200

    adds swift container plugin

--- Package.swift
+++ Package.swift
@@ -11,6 +11,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/apple/swift-container-plugin", from: "1.0.0"),
+        
     ],
     targets: [
         .executableTarget(
